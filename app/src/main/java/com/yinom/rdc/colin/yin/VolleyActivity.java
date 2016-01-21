4cf93c68a3c1109f3e1147ce3337d2712cac20b5
Simo YiN <yindp@outlook.com>
2016-01-21 16:04:55 +0800

    Volley

--- app/src/main/java/com/yinom/rdc/colin/yin/VolleyActivity.java
+++ app/src/main/java/com/yinom/rdc/colin/yin/VolleyActivity.java
@@ -11,10 +11,12 @@ import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.yinom.rdc.colin.yin.network.MyApplication;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -38,7 +40,7 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnTransferVolley:
-                StringRequestGet();
+                StringRequest_Get();
                 break;
         }
     }
@@ -47,7 +49,7 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
      * Volley StringRequest
      * GET method
      */
-    private void StringRequestGet() {
+    private void StringRequest_Get() {
         String url = "http://apis.juhe.cn/mobile/get?phone=" + ettPhoneNumber.getText().toString() +
                 "&key=1b27ee246b8392716f924692cc241d25";
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response
@@ -70,7 +72,7 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
      * Volley StringRequest
      * POST method
      */
-    private void StringRequestPost() {
+    private void StringRequest_Post() {
         String url = "http://apis.juhe.cn/mobile/get";
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response
                 .Listener<String>() {
@@ -98,7 +100,7 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
      * Volley JsonObjectGet
      * GET method
      */
-    private void JsonObjectGet() {
+    private void JsonObjectRequest_Get() {
         String url = "http://apis.juhe.cn/mobile/get?phone=" + ettPhoneNumber.getText().toString() +
                 "&key=1b27ee246b8392716f924692cc241d25";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, new
@@ -119,7 +121,7 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
      * Volley JsonObjectRequest
      * POST method
      */
-    private void JsonObjectPost() {
+    private void JsonObjectRequest_Post() {
         String url = "http://apis.juhe.cn/mobile/get";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, new
                 Response.Listener<JSONObject>() {
@@ -133,6 +135,54 @@ public class VolleyActivity extends AppCompatActivity implements View.OnClickLis
                 ttvNotice.setText(error.toString());
             }
         }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> hashMap = new HashMap<String, String>();
+                hashMap.put("phone", ettPhoneNumber.getText().toString());
+                hashMap.put("key", "1b27ee246b8392716f924692cc241d25");
+                return hashMap;
+            }
+        };
+    }
+
+    /**
+     * Volley JsonArrayRequest
+     * GET Method
+     */
+    private void JsonArrayRequest_Get() {
+        String url = "http://apis.juhe.cn/mobile/get?phone=" + ettPhoneNumber.getText().toString() +
+                "&key=1b27ee246b8392716f924692cc241d25";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, new
+                Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                ttvNotice.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                ttvNotice.setText(error.toString());
+            }
+        });
+    }
+
+    /**
+     * Volley JsonArrayRequest
+     * POST method
+     */
+    private void JsonArrayRequest_Post() {
+        String url="http://apis.juhe.cn/mobile/get";
+        JsonArrayRequest jsonArrayRequest=new JsonArrayRequest(Request.Method.POST, url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                ttvNotice.setText(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                ttvNotice.setText(error.toString());
+            }
+        }){
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> hashMap = new HashMap<String, String>();
